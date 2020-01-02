84649b256227fb52da1a71ec840b5025b6c3cf21
Yefei Chen <yefei.chen9@wework.com>
2020-01-02 14:00:05 +0800

    +v2 batch size

--- get_sub_recommend_reason_after_similarity_v2.py
+++ get_sub_recommend_reason_after_similarity_v2.py
@@ -150,6 +150,12 @@ if __name__ == '__main__':
             print('5. Similar company name')
             recall_com5 = sub_rec_similar_company_v2(comp_loc = comp_loc, sspd = sspd , thresh=0.05)
             sim_comp_name = recall_com5.get_reason(comp_feat=comp_feat,comp_feat_col=comp_feat_col,comp_feat_normed=comp_feat_normed,reason_col_name = reason5 )
+        elif cityname[ind_city] in ['New York']:
+            print('5. Similar company name')
+            recall_com5 = sub_rec_similar_company_v2(comp_loc=comp_loc, sspd=sspd, thresh=0.05)
+            sim_comp_name = recall_com5.get_reason_batch(comp_feat=comp_feat, comp_feat_col=comp_feat_col,
+                                                   comp_feat_normed=comp_feat_normed, reason_col_name=reason5,batch_size=5000)
+
 
         print('6. Close to current location')
         recall_com6 = sub_rec_location_distance(reason_col_name=reason6)
@@ -160,11 +166,14 @@ if __name__ == '__main__':
         sample_sspd = pd.merge(sample_sspd, sub_pairs, on=['atlas_location_uuid', 'duns_number'], how='left',
                                suffixes=['', '_right'])
         #2 merge company similarity reason
-        if args.sampled or (cityname[ind_city] not in ['New York']):
-            sample_sspd = pd.merge(sample_sspd, sim_comp_name, on=['atlas_location_uuid', 'duns_number'], how='left',
-                                   suffixes=['', '_right'])
-        else:
-            sample_sspd[reason5] = 'Similar company recommendation reason is skipped for New York.'
+        sample_sspd = pd.merge(sample_sspd, sim_comp_name, on=['atlas_location_uuid', 'duns_number'], how='left',
+                               suffixes=['', '_right'])
+        # if args.sampled or (cityname[ind_city] not in ['New York']):
+        #     sample_sspd = pd.merge(sample_sspd, sim_comp_name, on=['atlas_location_uuid', 'duns_number'], how='left',
+        #                            suffixes=['', '_right'])
+        # else:
+            # sample_sspd[reason5] = 'Similar company recommendation reason is skipped for New York.'
+
 
         # sample_sspd = sample_sspd[sample_sspd[reason1].notnull() | sample_sspd[reason5].notnull()]
         sample_sspd[[reason1, reason5]] = sample_sspd[[reason1, reason5]].fillna('')
